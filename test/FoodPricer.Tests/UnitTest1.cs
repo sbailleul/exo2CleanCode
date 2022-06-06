03c8cc7f7aa132f4d9f1253a9155b49d44e7e9f5
Rui Carvalho <rhwy@users.noreply.github.com>
2022-06-06 14:52:58 +0000

    clean up folders

--- test/FoodPricer.Tests/UnitTest1.cs
+++ test/FoodPricer.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Xunit;
-
+using System;
+using System.
 namespace FoodPricer.Tests;
 
 public class UnitTest1
@@ -7,6 +8,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+var price = new App().Compute("assiette", "couscous", "coca", "moyen", "baba", "normal", "yes");
+
+      using var writer = new StringWriter();
+      Console.SetOut(writer);
+      Console.SetError(writer);
+      Program.Main(new string[]{});
+      var sut = writer.ToString();
 
     }
 }
